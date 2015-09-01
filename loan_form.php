1e069239c38f0e26e1060153d9e80c8a47ce28b2
smilecs <mumene@gmail.com>
2015-09-01 11:31:56 +0100

    loan start

---
+++ loan_form.php
@@ -0,0 +1,9 @@
+<div class="form-group">
+  <label for="exampleInputEmail1">Email address</label>
+  <input type="email" name="email" class="form-control" id="exampleInputEmail1" value="<?php echo $email; ?>">
+<input type="text" name="phone" class="form-control" value="<?php echo $phone; ?>"/>
+</div>
+<div class="form-group">
+  <label for="address">Address</label>
+  <input type="text" id="address" name="address" class="form-control" value="<?php echo $address; ?>"/>
+</div>
